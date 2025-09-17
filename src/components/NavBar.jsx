52079ddc32fc301ee492d586baf3598e922b8a8e
RomanForDev <romanfortinidev@gmail.com>
2025-09-17 16:30:57 -0300

    base del componente NavBar.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+    return <nav>
+            <img src="./src/assets/react.svg" alt="logo" className="logo" />
+        <ul>
+            <h3>Productos</h3>
+            <h3>Sobre Nosotros</h3>
+            <h3>Contactanos!</h3>
+        </ul>
+        <span></span>
+    </nav>
+}
+export default NavBar;
