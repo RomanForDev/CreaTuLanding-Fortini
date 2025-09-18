36754ee4838fc423f2a94a2bdf31a1c02f14d0f2
RomanForDev <romanfortinidev@gmail.com>
2025-09-18 18:58:12 -0300

    Arreglo de estilos. Cambio de ul a li en navbar porque genera un margin que nadie pidió

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import CartWidget from './CartWidget';
 function NavBar() {
     return <nav>
             <img src="./src/assets/react.svg" alt="logo" className="logo" />
-        <ul>
+        <li className='navlist'>
             <h3 className='nav-btn'>Productos</h3>
             <h3 className='nav-btn'>Sobre Nosotros</h3>
             <h3 className='nav-btn'>Contactanos!</h3>
-        </ul>
+        </li>
         {/* Render del carrito con su número */}
         <CartWidget />
     </nav>
