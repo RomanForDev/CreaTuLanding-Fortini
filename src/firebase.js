6918f5558508b930c9238ca0f99c10aa647eabab
RomanForDev <romanfortinidev@gmail.com>
2025-11-07 19:25:42 -0300

    firestore conectado con env.

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,17 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.MI_API_KEY,
+  authDomain: process.env.MI_AUTH_DOMAIN,
+  projectId: process.env.MI_PROYECT_ID,
+  storageBucket: process.env.MI_STORAGE_BUCKET,
+  messagingSenderId: process.env.MI_MESSAGING_SENDER_ID,
+  appId: process.env.MI_APP_ID
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
