745468dc66798146b69c94b786eb868b5cfcfad6
RomanForDev <romanfortinidev@gmail.com>
2025-11-12 16:21:17 -0300

    corrección src imágenes en componentes. Empezada la funcionalidad del carrito

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 // import { getProductById } from "../../data/mockApiService";
 import { getProductById } from "../../data/firestoreService";
 import { useParams } from "react-router";
 import { Link } from 'react-router';
+import cartContext from "../../context/cartContext";
 
 function ItemDetail() {
     const [product, setProduct] = useState({});
     const { id } = useParams()
+    const [count, setCount] = useState(1)
+    const { addItem } = useContext(cartContext);
+    // const [maxValue, setMaxValue] = useState(0);
 
     useEffect(() => {
         getProductById(id)
@@ -20,6 +24,23 @@ function ItemDetail() {
         return <div>Cargando producto...</div>;
     }
 
+    // function checkAvalaible() {
+    //     getProductById( id );
+    //     if(id){ () => setMaxValue(id.rating.count)}
+    // }
+
+    function sumar(){
+    if (count < 0)
+        { setCount( count+1 ) }
+    // else {
+    //     setLimit(true)
+    // }
+    }
+
+    function addToCart(){
+    addItem( { ...product, quantity: count } )
+    }
+
     return (
         <div className="product-detail">
             <div className="card-detail">
@@ -31,7 +52,7 @@ function ItemDetail() {
                     <h4 className="prod-price-detail">${product.price}</h4>
                     <p className="prod-description-detail">{product.description}</p>
                     <button><Link to="/products">Volver</Link></button>
-                    <button>Agregar al carrito</button>
+                    <button onClick={addToCart}>Agregar al carrito</button> {/*Acá va la funcionalidad para agregar al carrito*/}
                 </div>
             </div>
         </div>
